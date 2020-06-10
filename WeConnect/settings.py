1ee896451cbd4e390ce22e3d1bee49760e8c3c26
Meet-Turakhia <turakhiameeturesh@gmail.com>
2020-06-10 15:49:03 +0530

    database connected and post table added

--- WeConnect/settings.py
+++ WeConnect/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'WeConnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'weconnect',
+        'USER': 'postgres', 
+        'PASSWORD': 'datameta8',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def dashboard (request):
-    return render (request, 'dashboard.html')
+    posts = Post.objects.all()
+    
+    return render (request, 'dashboard.html',{'posts':posts})

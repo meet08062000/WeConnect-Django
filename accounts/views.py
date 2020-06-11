d0a13dbddca01f2a954da1e79db90d05be7b6dc0
Meet Shah <meet8june@gmail.com>
2020-06-11 16:32:36 +0530

    signup added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    if(request.method == 'POST'):
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if(password1 == password2):
+            if(User.objects.filter(username=username).exists()):
+                messages.info(request, 'Username taken')
+                return redirect('/')
+            elif(User.objects.filter(email=email).exists()):
+                messages.info(request, 'email taken')
+                return redirect('/')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+
+                user.save()
+                messages.info(request, 'user created')
+                return redirect('/dashboard')
+        else:
+            messages.info(request, 'passwords dont match')
+            return redirect('/')
+
+    else:
+        return redirect('/')
--- mainApp/models.py
+++ mainApp/models.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     desc = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='post_images')
     timestamp = models.DateTimeField(default=timezone.now)
     tags = models.CharField(max_length=10)
     author = models.ForeignKey(User, on_delete=models.CASCADE)

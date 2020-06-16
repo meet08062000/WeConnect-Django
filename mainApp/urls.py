51522f6037e265f20d3b46ea517c1cd5abee2d71
Meet-Turakhia <turakhiameeturesh@gmail.com>
2020-06-16 16:06:43 +0530

    follow system

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('post', views.post, name="post"),
     path('profilepage', views.profilepage, name="profilepage"),
     path('profile/<int:user_id>', views.profile, name="profile"),
+    path('follows/<int:user_id>', views.follows, name="follows"),
     path('<int:post_id>', views.like, name="like")
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Post
-from .models import Likes
+from .models import Post, Likes, Follows
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
@@ -32,36 +31,55 @@ def post(request):
 
 
 def profilepage(request):
-    if (request.method == 'POST'):
+    if (request.method.lower() == 'post'):
         pass
     else:
         logged_in_user_posts = Post.objects.filter(author=request.user)
         posts_count = logged_in_user_posts.count()
         return render(request, 'profilepage.html', {'userposts': logged_in_user_posts, 'posts_count': posts_count})
 
+
 def like(request, post_id):
-    
-    like_count = Likes.objects.filter(user_id = request.user.id, post_id = post_id).count()
-    post= Post.objects.filter(id=post_id).get()  
-    if(like_count != 0):
-        post.likes -= 1
-        post.save()
-        like = Likes.objects.filter(user_id = request.user.id, post_id = post_id).get()
-        like.delete()
-    else:
-        like = Likes()
-        like.user_id = request.user.id
-        like.post_id = post_id
-        post.likes += 1
-        post.save()
-        like.save()
-    return redirect('/app')
+    if(request.method.lower() == "post"):
+        like_count = Likes.objects.filter(
+            user_id=request.user.id, post_id=post_id).count()
+        post = Post.objects.filter(id=post_id).get()
+        if(like_count != 0):
+            post.likes -= 1
+            post.save()
+            like = Likes.objects.filter(
+                user_id=request.user.id, post_id=post_id).get()
+            like.delete()
+        else:
+            like = Likes()
+            like.user_id = request.user.id
+            like.post_id = post_id
+            post.likes += 1
+            post.save()
+            like.save()
+        return redirect('/app')
+
 
 def profile(request, user_id):
     if(user_id != request.user.id):
         user = User.objects.filter(id=user_id).get()
-        user_posts = Post.objects.filter(author = user)
-        user_posts_count = Post.objects.filter(author = user).count()
+        user_posts = Post.objects.filter(author=user)
+        user_posts_count = Post.objects.filter(author=user).count()
         return render(request, 'profile.html', {'user': user, 'userposts': user_posts, 'posts_count': user_posts, 'posts_count': user_posts_count})
     else:
         return redirect('/app/profilepage')
+
+
+def follows(request, user_id):
+    if(request.method.lower() == "post"):
+        follow_count = Follows.objects.filter(
+            follower=request.user.id, receiver=user_id).count()
+        if (follow_count != 0):
+            follow = Follows.objects.filter(receiver=user_id).delete()
+            return redirect('/app/profile/{}'.format(user_id))
+        else:
+            follow = Follows()
+            follow.follower_id = request.user.id
+            follow.receiver_id = user_id
+            follow.save()
+            return redirect('/app/profile/{}'.format(user_id))

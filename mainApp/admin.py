ae55db2f90962204d2a1f90db6fefbe6f98e028c
Meet-Turakhia <turakhiameeturesh@gmail.com>
2020-06-16 16:29:46 +0530

    small hanges

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Likes, Follows
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Likes)
+admin.site.register(Follows)
